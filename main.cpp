2a367fb33212e5d226c1b5be493bfb6dd4836095
seba03n <115528575+seba03n@users.noreply.github.com>
2022-11-28 11:15:11 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,5 +2,23 @@
 using namespace std;
 
 int main() {
+     int n;
+    int p=0;
+    int s=1;
+    int i=1;
+    cin >> n;
+    cout << s <<endl;
+    while (i++ <= n){
+        cout << p+s << endl;
+        if(p<s){
+            p+=s;
+
+        }else{
+            s+=p;
+
+        }
+        if ((p+s)>n)
+            break;
+    }
    return 0;
 }
